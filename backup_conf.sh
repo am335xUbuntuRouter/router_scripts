a3aa88d4dfec29f09228be96574159323d6bfcb0
Yiling Cao <yiling.cao@gmail.com>
2017-07-06 10:28:21 +0000

    add new wlan0 config and ppp0 config

--- backup_conf.sh
+++ backup_conf.sh
@@ -1,2 +1,5 @@
 mkdir -p scripts/etc
+mkdir -p scripts/etc/lighttpd
+
 cp /etc/rc.local scripts/etc/rc.local
+cp -ar /etc/lighttpd/* scripts/etc/lighttpd
--- scripts/wlan0_connect_ap.sh
+++ scripts/wlan0_connect_ap.sh
@@ -1,3 +1,3 @@
 #atheros firmware: http://linuxwireless.org/download/htc_fw/1.3/htc_9271.fw
-wpa_supplicant -D nl80211 -i wlan0 -B -c /etc/wpa_supplicant/wpa_supplicant.conf
+wpa_supplicant -D nl80211 -i wlan0 -B -c /root/router_scripts/scripts/gntek2_wpa.conf
 dhclient wlan0
